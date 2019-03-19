4bc18ae1708d7f49cb9e65b2f28b7b06b7bff7c3
Max <47576240+maxx0911@users.noreply.github.com>
2019-03-19 21:24:53 +0100

    kick, ban and prune update
    should now work as intended. prune needs a better confirmation system, maybe on_reaction_add.

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -11,10 +11,11 @@ class moderation(commands.Cog):
 
     @commands.has_permissions(kick_members=True)
     @commands.command(pass_context=True)
-    async def kick(self, ctx, user: discord.Member):
+    async def kick(self, ctx, user: discord.Member, *, reason: str):
         await ctx.send(f"<@{user.id}> you will be kicked in 15 seconds. Any last words?")
         await asyncio.sleep(15)
-        await bot.kick(user)
+        await user.send(f"You've been kicked from {ctx.guild.name}. Reason: {reason}")
+        await user.kick()
         await ctx.send(f"{ctx.message.author} kicked {user}. See ya later!")
 
     @commands.has_permissions(ban_members=True)
@@ -22,41 +23,39 @@ class moderation(commands.Cog):
     async def ban(self, ctx, user: discord.Member):
         await ctx.send(f"<@{user.id}> you will be banned in 15 seconds. Any last words?")
         await asyncio.sleep(15)
-        await bot.ban(user)
+        await user.send(f"You've been banned from {ctx.guild.name}. Reason: {reason}")
+        await user.ban()
         await ctx.send(f"{ctx.message.author} banned {user}, never to be seen again!")
     
     @commands.has_permissions(manage_messages=True)
     @commands.command(pass_context=True, aliases=["delete","clean","clear"])
-    async def delet(self, ctx, amount=99):
+    async def delet(self, ctx, amount: int):
         if ctx.message.author.bot:
             return
-        channel = ctx.message.channel
-        messages = []
-        async for message in bot.logs_from(channel, limit=int(amount) + 1):
-            messages.append(message)
-        await bot.delete_messages(messages)
+        if amount <= 0 or amount >= 100:
+            await ctx.send("cannot delete less than 0 or more than 100 messages", delete_after=4)
+            return
+        try:     
+            await ctx.purge(limit=amount)
+        except:
+            await ctx.send(f"`{amount}` isn't a number", delete_after=4)
+            return
         await ctx.send("Yup, deleted a whopping **{}** messages for ya!".format(amount))
 
     @commands.has_permissions(kick_members=True)
-    @commands.command(pass_context=True)
+    @commands.group(pass_context=True, invoke_without_command=True)
     async def prune(self, ctx, time=30):
         if ctx.message.author.bot:
             return
-        y = "\U0001F1FE"
-        n = "\U0001F1F3"
-        est = await bot.estimate_pruned_members(server=ctx.message.server, days=30)
-        est_msg = await ctx.send("This will get rid of an estimated **{}** people. Are you sure you want to prune?\nY for yes / N for no".format(est))
-        await bot.add_reaction(est_msg, y) # emoji Y
-        await bot.add_reaction(est_msg, n) # emoji N
-        react = await bot.wait_for_reaction(emoji=[y,n], user=ctx.message.author, timeout=30)
-        if react.reaction.emoji == y:
-            number = await bot.prune_members(server=ctx.message.server, days=time)
-            await ctx.send("That should have gotten rid of **{}** inactive users. Check any log channels and contact Chippy#7628 if something went wrong.".format(number))
-        elif react.reaction.emoji == n:
-            await ctx.send("Operation cancelled.")
-            return
-        else:
-            ctx.send("something went wrong")
+        guild = ctx.guild
+        est = await guild.estimate_pruned_members(days=30)
+        est_msg = await ctx.send(f"This will get rid of an estimated **{est}** people. Are you sure you want to prune?\ntype `.prune confirm` to confirm.")
+        
+    @commands.has_permission(kick_members=True)    
+    @prune.command(pass_context=True)     
+        number = await guild.prune_members(days=time)
+        await ctx.send(f"That should have gotten rid of **{number}** inactive users. Check the audit log and contact Chippy#7628 or Paws#0001 if something went wrong.")
+
 
 def setup(bot):
     bot.add_cog(moderation(bot))
